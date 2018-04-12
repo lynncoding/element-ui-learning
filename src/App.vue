c090c6130992dc619974aced0c0b64199a84e39a
Lynn <lynnxtu@gmail.com>
2018-04-12 21:55:14 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-aside width="200px" height="100%"><Menu /></el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
 
 <script>
+import Menu from './components/menu.vue'
 export default {
-  name: 'App'
+  components: {
+    Menu
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
