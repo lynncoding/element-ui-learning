306e1b65fc8547bde505da0a2fcc5ce02e137105
杨玲 <lynnxtu@gmail.com>
2018-04-21 16:43:29 +0800

    Update

--- src/components/forms/multiple_cascader.vue
+++ src/components/forms/multiple_cascader.vue
@@ -1,6 +1,6 @@
 <template>
   <span class="multiple-cascader el-cascader" v-clickoutside="handleClickoutside">
-    <el-select @focus="handleFocus" v-model="selectedValues" multiple placeholder="请选择" ref="select" popper-class="multiple-cascader-popper">
+    <el-select @focus="handleFocus" v-model="selectValues" multiple ref="select" :placeholder="placeholder" popper-class="multiple-cascader-popper">
       <el-option
         v-for="item in selectOptions"
         :key="item.value"
@@ -10,59 +10,98 @@
     </el-select>
     <el-collapse-transition>
       <div v-show="menuVisible" @click="handleClick">
-        <ul class="multiple-cascader-ul">
-          <li class="multiple-cascader-li" v-for="item in options" :key="item.value" @mouseover="hoverRootNode(item)">
-            <el-checkbox :indeterminate="isIndeterminate">{{item.label}}</el-checkbox>
-          </li>
-        </ul>
-        <ul v-show="showSecondMenu" class="multiple-cascader-ul">
-          <li class="multiple-cascader-li" v-for="secondLevelNode in secondLevelNodes" :key="secondLevelNode.value" @mouseover="hoverSecondNode(secondLevelNode)">
-            <el-checkbox>{{secondLevelNode.label}}</el-checkbox>
-          </li>
-        </ul>
-        <ul v-show="showThirdMenu" class="multiple-cascader-ul">
-          <li class="multiple-cascader-li" v-for="thirdLevelNode in thirdLevelNodes" :key="thirdLevelNode.value">
-            <el-checkbox
-            >
-              {{thirdLevelNode.label}}
-            </el-checkbox>
-          </li>
-        </ul>
+        <div v-if="options&&options.length>0">
+          <ul class="multiple-cascader-ul">
+            <li class="multiple-cascader-li" v-for="item in options" :key="item.value" @mouseover="hoverRootNode(item)">
+              <el-checkbox :indeterminate="true" @change="changeCheckbox">{{item.label}}</el-checkbox>
+            </li>
+          </ul>
+          <ul v-show="showSecondMenu" class="multiple-cascader-ul">
+            <li class="multiple-cascader-li" v-for="secondLevelNode in secondLevelNodes" :key="secondLevelNode.value" @mouseover="hoverSecondNode(secondLevelNode)">
+              <el-checkbox @change="changeCheckbox">{{secondLevelNode.label}}</el-checkbox>
+            </li>
+          </ul>
+          <ul v-show="showThirdMenu" class="multiple-cascader-ul">
+            <li class="multiple-cascader-li" v-for="thirdLevelNode in thirdLevelNodes" :key="thirdLevelNode.value">
+              <el-checkbox
+                :value="checkValue(thirdLevelNode.value)"
+                @input="check(thirdLevelNode)"
+                @change="changeCheckbox"
+              >
+                {{thirdLevelNode.label}}
+              </el-checkbox>
+            </li>
+          </ul>
+        </div>
+        <div class="empty-panel" v-else>没有数据</div>
       </div>
     </el-collapse-transition>
   </span>
 </template>
 
 <script>
-  import Clickoutside from 'element-ui/src/utils/clickoutside';
-  export default {
+  import Clickoutside from 'element-ui/src/utils/clickoutside'
+export default {
     directives: { Clickoutside },
     props: {
       options: Array,
-      values: Array
+      values: {
+        type: Array,
+        default () {
+          return []
+        }
+      },
+      placeholder: {
+        type: String,
+        default () {
+          return '请选择'
+        }
+      }
     },
     data () {
       return {
-        selectedValues: [],
+        selectValues: [],
+        selectOptions: [{
+          value: 'xiaolv',
+          label: '效率'
+        }, {
+          value: 'kekong',
+          label: '可控'
+        }],
         menuVisible: false,
         mounted: false,
         secondLevelNodes: [],
         thirdLevelNodes: [],
         showSecondMenu: false,
         showThirdMenu: false,
-        isIndeterminate: false,
-        selectOptions: []
-
+        isIndeterminate: false
+      }
+    },
+    watch: {
+      values: {
+        immediate: true,
+        handler (n) {
+          this.selectValues = this.values ? [].concat(this.values) : []
+        }
+      },
+      menuVisible (value) {
+        if (!value) {
+          this.showSecondMenu = false
+          this.showThirdMenu = false
+        }
       }
     },
     methods: {
-      handleClick () {
-        this.$refs.select.focus();
+      $getValue () {
+        return this.myValue
       },
-      handleFocus() {
+      handleClick () {
+        this.$refs.select.focus()
+      },
+      handleFocus () {
         this.menuVisible = true
       },
-      handleBlur() {
+      handleBlur () {
         this.menuVisible = false
       },
       hoverRootNode (item) {
@@ -72,18 +111,25 @@
       hoverSecondNode (item) {
         this.showThirdMenu = true
         this.thirdLevelNodes = item.children
+        this.selectOptions = item.children
       },
-      handleClickoutside() {
-        this.menuVisible = false;
-      }
-    },
-    watch: {
-      menuVisible(value) {
-        if(!value) {
-          this.showSecondMenu = false
-          this.showThirdMenu = false
+      handleClickoutside () {
+        this.menuVisible = false
+      },
+      checkValue (value) {
+        return !!~this.selectValues.indexOf(value)
+      },
+      check (node) {
+        let index = this.selectValues.indexOf(node.value)
+        if (~index) {
+          this.selectValues.splice(index, 1)
+        } else {
+          this.selectValues.push(node.value)
         }
       },
+      changeCheckbox (val, e) {
+
+      }
     },
     computed: {
     },
@@ -94,8 +140,18 @@
 </script>
 <style lang="less">
 .multiple-cascader-popper {
-  display: none !important;
+  display: none;
   background-color: #fff;
+}
+.menu-item {
+  margin-top: 10px;
+  padding: 0px;
+  float: left;
+  list-style: none;
+  background-color: #fff;
+  border: 1px solid #e4e8f1;
+  width: 150px;
+  height: 200px;
 }
 .multiple-cascader {
   display: inline-block;
@@ -115,6 +171,15 @@
     &:hover {
       background-color:#f5f7fa;
     }
+  }
+  .empty-panel {
+    background-color: #fff;
+    border: 1px solid #e4e8f1;
+    width: 150px;
+    height: 200px;
+    margin-top: 10px;
+    padding: 10px;
+    color: #c0c4cc;
   }
 }
 </style>
